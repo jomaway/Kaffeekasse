335d0078264940728fa005e3e46bbe96535ae4cc
Andreas <andreas299792@web.de>
2020-12-10 09:25:28 +0100

    class Lehrer hinzugefügt mit += -=

--- lehrer.cpp
+++ lehrer.cpp
@@ -1,6 +1,20 @@
 #include "lehrer.h"
 
-Lehrer::Lehrer()
-{
 
-}
+Lehrer::Lehrer(QString v, QString n, double g)
+    {
+        this->vorname = v;
+        this->nachname = n;
+        this->guthaben = g;
+    }
+
+void Lehrer::setVname(QString v){this->vorname = v;}
+void Lehrer::setNname(QString n){this->nachname = n;}
+void Lehrer::setGuthaben(double g){this->guthaben = g;}
+
+QString Lehrer::getVname(){return this->vorname;}
+QString Lehrer::getNname(){return this->nachname;}
+double Lehrer::getGuthaben(){return this->guthaben;}
+
+void operator+=(Lehrer& l, double betrag){l.guthaben = l.guthaben + betrag;}
+void operator-=(Lehrer& l, double betrag){l.guthaben = l.guthaben - betrag;}
--- lehrer.h
+++ lehrer.h
@@ -1,11 +1,29 @@
 #ifndef LEHRER_H
 #define LEHRER_H
-
+#include <QString>
 
 class Lehrer
 {
+private:
+    QString vorname;
+    QString nachname;
+    double  guthaben;
+
 public:
-    Lehrer();
+    Lehrer(QString v, QString n, double g);
+    ~Lehrer(){}
+
+    void setVname(QString v);
+    void setNname(QString n);
+    void setGuthaben(double g);
+
+    QString getVname();
+    QString getNname();
+    double getGuthaben();
+
+    friend void operator+=(Lehrer& l, double betrag);
+    friend void operator-=(Lehrer& l, double betrag);
+
 };
 
 #endif // LEHRER_H
