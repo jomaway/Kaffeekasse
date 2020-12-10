4f25df8344ec81f46134ea48092a1114e3a22d21
Endlein <jakob.endlein@gmx.de>
2020-12-10 08:56:53 +0100

    Fensternamen geändert. Datei menü hinzugefügt mit Speichern und Laden. Funktionen Speichern und Laden teilweise implementiert.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +14,34 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_actionSpeichern_triggered()
+{
+    //Öffnet Fenster um den Speicherort und Namen einzugeben
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Speichern nach:"), "", tr("CSV Files(*.csv)"));
+    // überprüft ob ein Dateiname eingegeben wurde
+    if (fileName.isEmpty()) { return; }
+/*
+    if (model->saveTo(fileName)) {
+        // gibt in der statusbar aus das erfolgreich gespeichert wurde
+        this->ui->statusbar->showMessage(tr("Kaffeekasse gespeichert"));
+    } else {
+        // Wenn ein Fehler auftritt wird diese Nachricht ausgegeben
+        this->ui->statusbar->showMessage(tr("Kaffeekasse konnte nicht gespeichert werden"));
+    }
+    */
+}
+
+void MainWindow::on_actionLaden_triggered()
+{
+    //Öffnet Fenster um die zu Öffnende datei auszuwählen
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Kaffeekasse auswählen"), "", tr("CSV Files(*.csv)"));
+    // überprüft ob etwas ausgewählt wurde
+    if (fileName.isEmpty()) { return; }
+/*    if (model->openFrom(fileName)) {
+        this->ui->statusbar->showMessage(tr("Kaffeekasse geöffnet"));
+    } else {
+        this->ui->statusbar->showMessage(tr("Die ausgewählte Datei konnte nicht geöffnet werden:"));
+    }
+    */
+}
